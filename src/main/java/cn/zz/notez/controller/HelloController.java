bac697eff88eaf2ef694c157c062e444b38af1f7
zz <zy13474176527@gmail.com>
2018-10-09 09:35:24 +0800

    hello

--- src/main/java/cn/zz/notez/controller/HelloController.java
+++ src/main/java/cn/zz/notez/controller/HelloController.java
@@ -1,10 +1,22 @@
 package cn.zz.notez.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
 /**
  * TODO
  *
  * @author: zz
  * @date: 2018/10/8 15:13
  */
+@RestController
 public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index() {
+        System.out.println(new Date() + "=========Hello World!");
+        return "Hello World";
+    }
 }
